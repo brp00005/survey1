07a7a31fa23e1672bc10778b1b03e7eae3ae3eb2
newyearstag <78046309+newyearstag@users.noreply.github.com>
2023-11-16 15:25:25 -0500

    Create rust.rs

---
+++ rust.rs
@@ -0,0 +1,48 @@
+fn hypercake(n: i32, k: i32) -> i32 {
+	fn combination(n: i32, r: i32) -> i32{
+		fn factorial(x: i32) -> i32{
+            
+			if x == 0 || x== 1 {
+				1			
+			}
+			else {
+				x * factorial(x-1)
+			}
+		}
+
+		if r >= 0 && n>=r {
+			return factorial(n) / ( factorial(r) * factorial(n - r) );
+		}
+		else {
+			return 0;
+		}
+	}
+		
+	
+	let mut i = 0;
+	let mut sum = 0;
+	while i <= k {
+		sum += combination (n, i);
+		i += 1;
+	}
+	
+	return sum;
+}	
+
+fn main() {
+
+    // Get user input
+    println!("What are the number of cuts?");
+    let mut cuts = String::new();
+    std::io::stdin().read_line(&mut cuts).expect("Failed to read line");
+    let cuts: i32 = cuts.trim().parse().expect("Invalid input");
+
+    println!("What are the number of dimensions?");
+    let mut dimensions = String::new();
+    std::io::stdin().read_line(&mut dimensions).expect("Failed to read line");
+    let dimensions: i32 = dimensions.trim().parse().expect("Invalid input");
+
+
+	let result = hypercake(cuts, dimensions);
+	println!("The result is: {}", result);
+}
