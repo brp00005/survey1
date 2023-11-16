2e771adcca2d0bf8d0af20e935b5c40ad3159a97
newyearstag <78046309+newyearstag@users.noreply.github.com>
2023-11-16 16:03:01 -0500

    Update rust.rs
    Comments

--- rust.rs
+++ rust.rs
@@ -1,7 +1,12 @@
+//@author Robert Coleman
+// I understand academic integrity
+// This JavaScript file calculates the hypercake of user input dimensions
+
+// "-> i32" bounds the return type of the function to a 32 bit integer
 fn hypercake(n: i32, k: i32) -> i32 {
 	fn combination(n: i32, r: i32) -> i32{
 		fn factorial(x: i32) -> i32{
-            
+            		// no need to say return in rust syntax
 			if x == 0 || x== 1 {
 				1			
 			}
@@ -9,7 +14,7 @@ fn hypercake(n: i32, k: i32) -> i32 {
 				x * factorial(x-1)
 			}
 		}
-
+			// also showcasing return works
 		if r >= 0 && n>=r {
 			return factorial(n) / ( factorial(r) * factorial(n - r) );
 		}
@@ -18,7 +23,7 @@ fn hypercake(n: i32, k: i32) -> i32 {
 		}
 	}
 		
-	
+	// i and sum must be mutable since they become dynamic variables
 	let mut i = 0;
 	let mut sum = 0;
 	while i <= k {
@@ -33,15 +38,17 @@ fn main() {
 
     // Get user input
     println!("What are the number of cuts?");
+    // declaring the input
     let mut cuts = String::new();
+    // reading the input
     std::io::stdin().read_line(&mut cuts).expect("Failed to read line");
+    // parsing input to 32 bit
     let cuts: i32 = cuts.trim().parse().expect("Invalid input");
 
     println!("What are the number of dimensions?");
     let mut dimensions = String::new();
     std::io::stdin().read_line(&mut dimensions).expect("Failed to read line");
     let dimensions: i32 = dimensions.trim().parse().expect("Invalid input");
-
 
 	let result = hypercake(cuts, dimensions);
 	println!("The result is: {}", result);
